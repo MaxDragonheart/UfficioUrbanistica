edb17a76b1500646a8328c16d0e66423b0aafa5d
MaxDragonheart <massimilianomoraca@gmail.com>
2022-09-22 11:37:43 +0200

    feat section stuffs #2

--- project/project/sections/urls.py
+++ project/project/sections/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('', views.tmp_homepage, name='temporary_homepage'),
     path('', views.homepage, name='homepage'),
-    path('<slug:slug_category>/', views.single_category, name='single_section'),
+    path('<slug:slug_category>/', views.single_category, name='single_category'),
+    path('<slug:slug_category>/<slug:slug_post>/', views.single_post, name='single_article'),
 ]
