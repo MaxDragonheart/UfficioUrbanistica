1083a4c60dcdf4434ff9a0a092e341f8f11bdc28
MaxDragonheart <massimilianomoraca@gmail.com>
2022-09-22 17:13:52 +0200

    feat create backup and restore scripts #11

--- project/scripts/local_backup_fixtures.sh
+++ project/scripts/local_backup_fixtures.sh
@@ -3,19 +3,14 @@
 echo "Start backup"
 echo "NOTE: use only in staging!!"
 
-# Standard Apps
-echo "- make fixtures for Django's standard apps"
-poetry run python3 ../project/manage.py dumpdata sites --indent 2 > ../project/fixtures/sites.json
-poetry run python3 ../project/manage.py dumpdata flatpages --indent 2 > ../project/fixtures/flatpages.json
-
 # Usermanager
 echo "- make fixtures for Usermanager"
 poetry run python3 ../project/manage.py dumpdata usermanager.userprofile --indent 2 > ../project/fixtures/usermanager/userprofile.json
 
-# Sections
-echo "- make fixtures for Sections"
-poetry run python3 ../project/manage.py dumpdata sections.section --indent 2 > ../project/fixtures/sections/section.json
-poetry run python3 ../project/manage.py dumpdata sections.sectionpost --indent 2 > ../project/fixtures/sections/sectionpost.json
+# Standard Apps
+echo "- make fixtures for Django's standard apps"
+poetry run python3 ../project/manage.py dumpdata sites --indent 2 > ../project/fixtures/sites.json
+poetry run python3 ../project/manage.py dumpdata flatpages --indent 2 > ../project/fixtures/flatpages.json
 
 # Link
 echo "- make fixtures for Link"
@@ -24,5 +19,10 @@ poetry run python3 ../project/manage.py dumpdata link.link --indent 2 > ../proje
 # Media
 echo "- make fixtures for Media"
 poetry run python3 ../project/manage.py dumpdata media.fileupload --indent 2 > ../project/fixtures/media/fileupload.json
+
+# Sections
+echo "- make fixtures for Sections"
+poetry run python3 ../project/manage.py dumpdata sections.section --indent 2 > ../project/fixtures/sections/section.json
+poetry run python3 ../project/manage.py dumpdata sections.sectionpost --indent 2 > ../project/fixtures/sections/sectionpost.json
 
 echo "End process"
