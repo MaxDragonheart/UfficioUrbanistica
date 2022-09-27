a3ac5829ab41a9fe778628f66fb60fe1cefd93e4
MaxDragonheart <massimilianomoraca@gmail.com>
2022-09-27 10:15:39 +0200

    feat opengeodata integration #19

---
+++ project/project/static/js/commons.js
@@ -0,0 +1,25 @@
+function copyToClipboard(id) {
+  /* Get the text field */
+  var copyText = document.getElementById(id);
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function showHide(objectID) {
+  var x = document.getElementById(objectID);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else if (x.style.display != "block") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
