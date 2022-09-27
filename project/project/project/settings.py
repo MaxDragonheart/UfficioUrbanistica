cdb7549ae03578955369c018cda2f3f320c52aef
MaxDragonheart <massimilianomoraca@gmail.com>
2022-09-27 16:33:49 +0200

    feat opengeodata integration #19

--- project/project/project/settings.py
+++ project/project/project/settings.py
@@ -152,7 +152,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-DECIMAL_SEPARATOR = ','
+DECIMAL_SEPARATOR = '.'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
