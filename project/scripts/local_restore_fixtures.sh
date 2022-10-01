eeae82c3f1a4fb813652b681d2961feffcd8b22a
MaxDragonheart <massimilianomoraca@gmail.com>
2022-10-01 12:59:51 +0200

    feat dockerize project #25

--- project/scripts/local_restore_fixtures.sh
+++ project/scripts/local_restore_fixtures.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 # Useful only in development on local machine.
-echo "Start restore"
-echo "NOTE: use only in development!!"
+echo "- Start restore"
+echo "NOTE: use this process only in development!!"
 
 # Usermanager
-echo "- restore fixtures for Usermanager"
+echo "1. Restore fixtures for Usermanager"
 python3 ../project/manage.py loaddata ../project/fixtures/usermanager/userprofile.json
 
 # Standard Apps
-echo "- restore fixtures for Django's standard apps"
+echo "2. Restore fixtures for Django's standard apps"
 python3 ../project/manage.py loaddata ../project/fixtures/sites.json
 python3 ../project/manage.py loaddata ../project/fixtures/flatpages.json
 
 # Core
-echo "- restore fixtures for Core app"
+echo "3. Restore fixtures for Core app"
 python3 ../project/manage.py loaddata ../project/fixtures/core/sitecustomization.json
 
 # Link
-echo "- restore fixtures for Link app"
+echo "4. Restore fixtures for Link app"
 python3 ../project/manage.py loaddata ../project/fixtures/link/link.json
 
 # Media
-echo "- restore fixtures for Media app"
+echo "5. Restore fixtures for Media app"
 python3 ../project/manage.py loaddata ../project/fixtures/media/fileupload.json
 
 # Sections
-echo "- restore fixtures for Sections app"
+echo "6. Restore fixtures for Sections app"
 python3 ../project/manage.py loaddata ../project/fixtures/sections/section.json
 python3 ../project/manage.py loaddata ../project/fixtures/sections/sectionpost.json
 
 # Open GeoData
-echo "- restore fixtures for Open GeoData app"
+echo "7. Restore fixtures for Open GeoData app"
 python3 ../project/manage.py loaddata ../project/fixtures/opengeodata/categories.json
 python3 ../project/manage.py loaddata ../project/fixtures/opengeodata/geoserverdomain.json
 python3 ../project/manage.py loaddata ../project/fixtures/opengeodata/geoserverworkspace.json
