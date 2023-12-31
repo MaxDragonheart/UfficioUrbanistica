c1d84368eb01a43fbd5732148c063c5ed5770073
MaxDragonheart <massimilianomoraca@gmail.com>
2023-12-31 15:58:05 +0100

    feat upgrade packages #41

--- project/scripts/staging/create_env_file.sh
+++ project/scripts/staging/create_env_file.sh
@@ -12,7 +12,7 @@ touch $FILE_PATH
 
 echo "SECRET_KEY='$CREATE_KEY'" >> $FILE_PATH
 echo "DB_ENGINE=django.contrib.gis.db.backends.postgis" >> $FILE_PATH
-echo "DEBUG=0" >> $FILE_PATH
+echo "DEBUG=1" >> $FILE_PATH
 echo "DB_NAME=$DB_NAME" >> $FILE_PATH
 echo "DB_USER=postgres" >> $FILE_PATH
 echo "DB_PASSWORD=1983" >> $FILE_PATH
